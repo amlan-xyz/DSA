63027571eae9ed977c8e88e193aaca9b200323e2
Amlanjyoti Saikia <heyamlan19@gmail.com>
2022-12-25 11:56:09 +0530

    completed : backtracking

---
+++ Backtracking/rat_in_a_maze.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void ratInAMaze(int maze[][20],int n){
+	int** solution=new int*[n];
+
+	for(int i=0;i<n;i++){
+		solution[i]=new int*[n];
+	}
+
+}
+
+int main(){
+
+
+	return 0;
+}
